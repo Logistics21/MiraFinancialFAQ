44562545ad99016ff312c50cc8f4c42cb36c4349
David Halsey <dhalsey212@gmail.com>
2018-02-01 12:21:33 -0500

    Add ArticleItem

--- src/articleItem.jsx
+++ src/articleItem.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const ArticleItem = ({ article }) => (
+  <li onClick={(e) => expandArticle(e)} className="article-index-item">
+    <h5>{article.title}</h5>
+    <p><img src={article.authorImg} /> Written by {article.author}</p>
+    <p>{article.body}</p>
+  </li>
+)
+
+export default ArticleItem;
